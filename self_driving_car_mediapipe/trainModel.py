9991e3d3107c8947b53ded780e68b2079b20e405
dudrhkd9005 <dudrhkd75@gmail.com>
2021-05-09 19:44:12 +0900

    Update trainModel.py

--- self_driving_car_mediapipe/trainModel.py
+++ self_driving_car_mediapipe/trainModel.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import accuracy_score
 import pickle
 
 
@@ -14,8 +13,6 @@ if __name__ == '__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1234)
     model = make_pipeline(StandardScaler(), LogisticRegression()).fit(x_train, y_train)
-    predict = model.predict(x_test)
-    accuracy = accuracy_score(y_test, predict)
 
     with open("train.model", 'wb') as f:
         pickle.dump(model,f)
